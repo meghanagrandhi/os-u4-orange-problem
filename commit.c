b31f92d8cf5f9e237c054b21303e768588bea9ed
meghanagrandhi <mailtomeghana.g@gmail.com>
2026-04-21 10:11:10 +0530

    Implement commit creation in commit.c
    Implement commit creation logic to serialize and write a new commit object.

--- commit.c
+++ commit.c
@@ -194,8 +194,46 @@ int head_update(const ObjectID *new_commit) {
 //
 // Returns 0 on success, -1 on error.
 int commit_create(const char *message, ObjectID *commit_id_out) {
-    // TODO: Implement commit creation
-    // (See Lab Appendix for logical steps)
-    (void)message; (void)commit_id_out;
-    return -1;
+    ObjectID tree_id;
+    if (tree_from_index(&tree_id) != 0)
+        return -1;
+
+    Commit c;
+    memset(&c, 0, sizeof(c));
+
+    c.tree = tree_id;
+
+    ObjectID parent;
+    if (head_read(&parent) == 0) {
+        c.parent = parent;
+        c.has_parent = 1;
+    } else {
+        c.has_parent = 0;
+    }
+
+    snprintf(c.author, sizeof(c.author), "%s", pes_author());
+    c.timestamp = (uint64_t)time(NULL);
+    snprintf(c.message, sizeof(c.message), "%s", message);
+
+    void *raw;
+    size_t len;
+
+    if (commit_serialize(&c, &raw, &len) != 0)
+        return -1;
+
+    ObjectID cid;
+    if (object_write(OBJ_COMMIT, raw, len, &cid) != 0) {
+        free(raw);
+        return -1;
+    }
+
+    free(raw);
+
+    if (head_update(&cid) != 0)
+        return -1;
+
+    if (commit_id_out)
+        *commit_id_out = cid;
+
+    return 0;
 }
